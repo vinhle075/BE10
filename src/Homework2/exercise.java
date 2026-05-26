c65d06f10ff3bc3333b37579bc32f7b6e6b3de59
vinhle075 <vinhle075@gmail.com>
2026-05-26 18:58:10 +1000

    homework

--- src/Homework2/exercise.java
+++ src/Homework2/exercise.java
@@ -80,8 +80,7 @@ public class exercise {
 //	}
 //}
 
-
-	    public static int singleNumber(int[] nums) {
+public static int singleNumber(int[] nums) {
 			Arrays.sort(nums);
 			for(int i = 1; i < nums.length; i +=2) {
 				if (nums[i-1]!=nums[i]) {

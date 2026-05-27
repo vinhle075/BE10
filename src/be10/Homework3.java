6c7d7c0bf72c0efff5ab5cd397a918b79bd8251a
vinhle075 <vinhle075@gmail.com>
2026-05-27 19:52:31 +1000

    homework

--- src/be10/Homework3.java
+++ src/be10/Homework3.java
@@ -1,6 +1,6 @@
 package be10;
 public class Homework3 {
-	public static int binaryGap(int n) {
+/*	public static int binaryGap(int n) {
 		    n >>= Integer.numberOfTrailingZeros(n); 
 		    if (n == 1) 
 		    	return 0;                   
@@ -23,3 +23,43 @@ public class Homework3 {
 		System.out.println(d);
 	}
 }
+*/
+//	Sai
+	public int solution(int[] A) {
+	    long S = 0;
+
+	    for (int i = 1; i < A.length - 1; i++) {
+	        int prev    = A[i - 1];   //
+	        int current = A[i];
+	        int next    = A[i + 1];
+
+	        // Skip số trùng liên tiếp
+	        while (i < A.length - 1 && A[i] == A[i + 1]) {
+	            i++;
+	        }
+
+	        if (i != A.length - 1) {
+	            next = A[i + 1];
+	        }
+
+	        if (current > prev && current > next) {
+	            S += current;   // đỉnh → cộng
+	        } else if (current < prev && current < next) {
+	            S -= current;   // đáy → trừ
+	        }
+	    }
+
+	    // Xử lý phần tử đầu và cuối
+	    if (A[0] < A[1])                         
+	    	S -= A[0]; // đáy đầu
+	    if (A[0] > A[1])                         
+	    	S += A[0]; // đỉnh đầu
+	    if (A[A.length-1] > A[A.length-2])       
+	    	S += A[A.length-1]; // đỉnh cuối
+	    if (A[A.length-1] < A[A.length-2])       
+	    	S -= A[A.length-1]; // đáy cuối
+
+	    return (int) (S % MOD);
+	}
+}
+	        

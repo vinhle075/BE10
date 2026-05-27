7c468a655fe05e9fac02500dcd9fb9a4e23f4c03
vinhle075 <vinhle075@gmail.com>
2026-05-27 17:35:01 +1000

    homework

--- src/be10/Homework3.java
+++ src/be10/Homework3.java
@@ -1,39 +1,25 @@
 package be10;
-
 public class Homework3 {
-	   public static int binaryGap(int n) {
-	        if(n <= 2) {
-	            return 0;
-	        }
-	        String num = Integer.toBinaryString(n); 
-	    
-	        int l = num.length();
-	        int maxDist = 0;
-	        int dist = 0;
-	        
-	        for(int i=1; i<l; i++) {
-	            char digit = num.charAt(i);
-	            if(digit == '1') {
-	                maxDist = Math.max(dist, maxDist);
-	                dist = 1;
-	            } 
-	            else {
-	                dist++;
-	            }
-	        }
-	        return maxDist;
-	    }
-		public static void main(String[] args) {
-		System.out.println(binaryGap(1041));
+	public static int binaryGap(int n) {
+		    n >>= Integer.numberOfTrailingZeros(n); //remove trailing zeros
+		    if (n == 1) 
+		    	return 0;                   //n is a power of two
+
+		    int gap = 0;
+		    int maxGap = 0;
+		    while (n > 0) {
+		        if ((n & 1) == 1) {                 //found set bit
+		            maxGap = Math.max(maxGap, gap);
+		            gap = 0;                        //reset gap
+		        } else
+		            gap++;                          //increase gap
+		        n >>= 1;                            //advance to next bit
+		    }
+		    return maxGap;
 		}
+	public static void main(String[] args) 
+	{
+		int d = binaryGap (32);
+		System.out.println(d);
 	}
-	
-
-	        
-//	        public static void main(String[] args) {
-//	        String num = Integer.toBinaryString(32);
-//	        System.out.println(num);
-//	        }
-//	  }
-
-	     
+}

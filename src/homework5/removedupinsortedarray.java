674bd4c8e867b2aeafd7192207a29c303fbda3bd
vinhle075 <email_cua_vinhle075@gmail.com>
2026-06-03 17:56:18 +1000

    homework

--- src/homework5/removedupinsortedarray.java
+++ src/homework5/removedupinsortedarray.java
@@ -1,5 +1,16 @@
 package homework5;
 
 public class removedupinsortedarray {
+	 public int removeDuplicates(int[] nums) {
+	        if(nums.length==0) return 0;
+	        int r=1;
+	        for(int l=1;l<nums.length;l++){
+	            if(nums[l]!=nums[l-1]){
+	                nums[r]=nums[r];
+	                r++;
+	            }
+	        }
+	        return r;
+	    }
+	}
 
-}

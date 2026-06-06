2f3ed7e509cbc1c697ff43ad874b26cd93e5c6d8
vinhle075 <email_cua_vinhle075@gmail.com>
2026-06-06 15:52:13 +1000

    homework

--- src/homework5/removedupinsortedarray.java
+++ src/homework5/removedupinsortedarray.java
@@ -1,16 +1,25 @@
 package homework5;
-
+import java.util.Arrays;
 public class removedupinsortedarray {
-	 public int removeDuplicates(int[] nums) {
-	        if(nums.length==0) return 0;
+	 public static int[] removeDuplicates(int[] nums) {
+	        if(nums.length==0) 
+	        {
+	        	return new int [0];
+	        };
 	        int r=1;
 	        for(int l=1;l<nums.length;l++){
 	            if(nums[l]!=nums[l-1]){
-	                nums[r]=nums[r];
+	                nums[r]=nums[l];
 	                r++;
 	            }
 	        }
-	        return r;
+	        return Arrays.copyOf(nums, r) ;
 	    }
+	public static void main(String[] args) {
+		int [] a=  {0,0,1,2,2,3,3,4};
+		System.out.println(Arrays.toString(removeDuplicates(a))); 
+		
+	}
+
 	}
 
